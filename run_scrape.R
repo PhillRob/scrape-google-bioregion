b36fc39faa40563e552f2132512e1a39d0741570
Philipp R <PhillRob@users.noreply.github.com>
2017-07-16 22:30:42 +1000

    Update run_scrape.R

--- run_scrape.R
+++ run_scrape.R
@@ -1,8 +1,10 @@
-##run scraping
-#source ("scrape.r")
+## run scraping
 source ("scrape.native.r")
-x<-read.csv("species_au_lag.csv"); x<-x[,2]
-a<-scrape.native (x,keyword=" native ")
 
+x <- read.csv("species_au_lag.csv"); x<-x[,2]
+a <- scrape.native (x,keyword=" native ")
 
-x1<-x[1:50]; x2<-x1[grepl("alopecurus pratensis",x1)]; x<-x2
+### just get a few species for testing
+x1 <- x[1:50]
+x2 <- x1[grepl("alopecurus pratensis", x1 )]
+x <- x2
