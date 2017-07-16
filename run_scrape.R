2f45760767ffe03d8a05617128c0e6f55ed00c8a
Philipp R <PhillRob@users.noreply.github.com>
2017-07-16 22:32:22 +1000

    Update run_scrape.R

--- run_scrape.R
+++ run_scrape.R
@@ -1,8 +1,8 @@
 ## run scraping
-source ("scrape.native.r")
+source ("scrape_native.r")
 
 x <- read.csv("species_au_lag.csv"); x<-x[,2]
-a <- scrape.native (x,keyword=" native ")
+a <- scrape_native (x,keyword=" native ")
 
 ### just get a few species for testing
 x1 <- x[1:50]
